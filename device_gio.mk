ef3bb350310110b51741f86cd9942cb3a9ed7174
OldDroid <pohl1998@gmx.de>
2013-01-13 16:41:16 +0100

    first commit

---
+++ device_gio.mk
@@ -0,0 +1,54 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## Inherit vendor proprietary files
+$(call inherit-product, vendor/samsung/gio/vendor_blobs.mk)
+
+include device/samsung/msm7x27-common/common.mk
+
+## Device specific overlay & kernel
+DEVICE_PACKAGE_OVERLAYS := device/samsung/gio/overlay
+TARGET_PREBUILT_KERNEL := device/samsung/prebuilt/kernel
+
+## Audio
+PRODUCT_PACKAGES += \
+    audio.primary.gio \
+    audio_policy.gio
+
+## Camera
+PRODUCT_PACKAGES += \
+    camera.gio \
+    libjni_legacymosaic \
+    LegacyCamera
+
+## Liblights
+PRODUCT_PACKAGES += \
+    lights.gio
+
+## Wifi
+#PRODUCT_PACKAGES += \
+#    abtfilt \
+#    wlan_tool \
+#    wmiconfig \
+    eeprom-AR6002 \
+    ar6000.ko
+
+## Ramdisk
+PRODUCT_COPY_FILES += \
+    device/samsung/gio/ramdisk/init.gt-s5660board.rc:root/init.gt-s5660board.rc \
+    device/samsung/gio/ramdisk/ueventd.gt-s5660board.rc:root/ueventd.gt-s5660board.rc \
+    device/samsung/gio/ramdisk/GIO.rle:root/GIO.rle
+
+## LDPI assets
+PRODUCT_AAPT_PREF_CONFIG := mdpi
---
+++ jbmp.mk
@@ -0,0 +1,26 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit some common JBMiniProject and CM stuff.
+$(call inherit-product, vendor/jbmp/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/gio/full_gio.mk)
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := GalaxyGio
+PRODUCT_VERSION_DEVICE_SPECIFIC := -GT-S5660
+-include vendor/cm/config/common_versions.mk
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := gio
+PRODUCT_NAME := jbmp_gio
+
+# Release data
+PRODUCT_VERSION_DEVICE_SPECIFIC := 2
+TARGET_NO_LIVEWALLPAPERS := false
+TARGET_BOOTANIMATION_NAME := vertical-320x480
+TARGET_INCLUDE_GAPPS := true
+
+## Bootanimation
+TARGET_BOOTANIMATION_NAME := vertical-320x480
